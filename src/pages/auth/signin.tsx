0c67999ad7b1a335002c37faff346ad622696889
BlueUpCode <88095362+blueupcode@users.noreply.github.com>
2024-07-16 17:20:12 +0700

    Update signin.tsx

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,28 +1,19 @@
+// pages/auth/signin.tsx
 import { NextPage } from "next";
-import { getProviders, signIn, LiteralUnion, ClientSafeProvider } from "next-auth/react";
-import { BuiltInProviderType } from "next-auth/providers/index";
+import { signIn } from "next-auth/react";
 import withGuest from "../../hoc/withGuest";
 
-type SignInProps = {
-  providers: Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null;
-};
-
-const SignIn: NextPage<SignInProps> = ({ providers }) => {
+const SignIn: NextPage = () => {
   return (
     <div>
       <h1>Sign in</h1>
-      {providers && Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>Sign in with {provider.name}</button>
-        </div>
-      ))}
+      <div>
+        <button onClick={() => signIn("twitter")}>
+          Sign in with Twitter
+        </button>
+      </div>
     </div>
   );
-};
-
-SignIn.getInitialProps = async (context) => {
-  const providers = await getProviders();
-  return { providers };
 };
 
 export default withGuest(SignIn);
