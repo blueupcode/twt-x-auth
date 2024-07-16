3d9e814b73ad8648bfde972102c2b9c3d2925c4b
BlueUpCode <88095362+blueupcode@users.noreply.github.com>
2024-07-16 17:41:36 +0700

    Add files via upload

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,11 @@
 import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
 
-console.log(process.env)
-
 export default NextAuth({
   providers: [
     TwitterProvider({
-      clientId: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID!,
-      clientSecret: process.env.NEXT_PUBLIC_TWITTER_CLIENT_SECRET!,
+      clientId: process.env.TWITTER_CLIENT_ID!,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
       version: "2.0", // Required for Twitter OAuth 2.0
     }),
   ],
